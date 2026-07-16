2faa7c91168b451abb9800258315c8f8410dbfba
Arthur Soares <148705578+arthursoares2509@users.noreply.github.com>
2026-07-16 16:53:22 -0300

    Bird Watcher

---
+++ tracks/Javascript/bird-watcher.js
@@ -0,0 +1,45 @@
+// @ts-check
+//
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  let total = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    total += birdsPerDay[i];
+  }
+  return total;
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  const startIndex = (week - 1) * 7;
+  const endIndex = startIndex + 7;
+  let total = 0;
+  for (let i = startIndex; i < endIndex; i++) {
+    total += birdsPerDay[i];
+  }
+  return total;
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {void} should not return anything
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i += 2) {
+    birdsPerDay[i] += 1;
+  }
+}
